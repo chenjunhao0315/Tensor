4450d5f8cf6c05682e11149a330d9d895266ada7
chenjunhao0315 <duncan11514@gmail.com>
2022-01-29 13:04:43 +0800

    Initial Commit

---
+++ Tensor/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Tensor
+//
+//  Created by 陳均豪 on 2022/1/29.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
