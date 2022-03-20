35f087e9483a58a6ba582616597d7080b52527a2
chenjunhao0315 <duncan11514@gmail.com>
2022-03-20 21:09:39 +0800

    Threshold native, documentation move to wiki

--- Tensor/ImageThreshold.cpp
+++ Tensor/ImageThreshold.cpp
@@ -21,7 +21,7 @@ Tensor threshold_binary(const Tensor& self, double threshold, double true_value,
 Tensor threshold(const Tensor& self, double threshold, double maxval, int type) {
     switch (type) {
         case THRESH_BINARY:
-            return threshold_binary(self, threshold, 0, maxval);
+            return threshold_binary(self, threshold, maxval, 0);
             break;
         case THRESH_TRUNC:
             return otter::native::threshold(self, threshold, maxval);
@@ -40,16 +40,35 @@ Tensor threshold_binary(const Tensor& self, double threshold, double true_value,
     int num_pixels = (int)(self.size(0) * self.size(1));
     
     if (self.scalar_type() == otter::ScalarType::Byte) {
+        unsigned char *src = self.data_ptr<unsigned char>();
+        unsigned char *ptr = out.data_ptr<unsigned char>();
+        
 #if __ARM_NEON
-
+        
 #else
         int remain = num_pixels;
 #endif  // __ARM_NEON
         
         if (true_value == 255 && false_value == 0) {
+#if __ARM_NEON
             
+#endif
+            for ( ; remain > 0; --remain) {
+                *ptr = (*src > threshold) ? 255 : 0;
+                
+                ++src;
+                ++ptr;
+            }
         } else {
+#if __ARM_NEON
             
+#endif
+            for ( ; remain > 0; --remain) {
+                *ptr = (*src > threshold) ? true_value : false_value;
+                
+                ++src;
+                ++ptr;
+            }
         }
     } else {
         
