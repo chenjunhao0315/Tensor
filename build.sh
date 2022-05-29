b93b2951a55b36c756d764783140af5727136a71
chenjunhao0315 <duncan11514@gmail.com>
2022-05-29 20:02:06 +0800

    build.sh update

--- build.sh
+++ build.sh
@@ -26,15 +26,21 @@ cmake --build . -j 8
 cmake --build . --target install
 popd
 
+mkdir -p framework
+pushd framework
+mkdir -p ios
+pushd ios
 mkdir -p otter.framework/Versions/A/Headers
 mkdir -p otter.framework/Versions/A/Resources
 ln -s A otter.framework/Versions/Current
 ln -s Versions/Current/Headers otter.framework/Headers
 ln -s Versions/Current/Resources otter.framework/Resources
 ln -s Versions/Current/otter otter.framework/otter
-lipo -create build-ios/install/lib/libotter.a build-ios-sim/install/lib/libotter.a -o otter.framework/Versions/A/otter
-cp -r build-ios/install/include/* otter.framework/Versions/A/Headers/
+lipo -create ../../build-ios/install/lib/libotter.a ../../build-ios-sim/install/lib/libotter.a -o otter.framework/Versions/A/otter
+cp -r ../../build-ios/install/include/* otter.framework/Versions/A/Headers/
 sed -e 's/__NAME__/otter/g' -e 's/__IDENTIFIER__/com.duncan.otter/g' -e 's/__VERSION__/1.0/g' Info.plist > otter.framework/Versions/A/Resources/Info.plist
+popd
+popd
 
 mkdir -p build-mac
 pushd build-mac
@@ -43,9 +49,41 @@ cmake --build . -j 8
 cmake --build . --target install
 popd
 
+mkdir -p framework
+pushd framework
+mkdir -p mac
+pushd mac
+mkdir -p otter.framework/Versions/A/Headers
+mkdir -p otter.framework/Versions/A/Resources
+ln -s A otter.framework/Versions/Current
+ln -s Versions/Current/Headers otter.framework/Headers
+ln -s Versions/Current/Resources otter.framework/Resources
+ln -s Versions/Current/otter otter.framework/otter
+lipo -create ../../build-mac/install/lib/libotter.a -o otter.framework/Versions/A/otter
+cp -r ../../build-mac/install/include/* otter.framework/Versions/A/Headers/
+sed -e 's/__NAME__/otter/g' -e 's/__IDENTIFIER__/com.duncan.otter/g' -e 's/__VERSION__/1.0/g' Info.plist > otter.framework/Versions/A/Resources/Info.plist
+popd
+popd
+
 mkdir -p build-mac-arm
 pushd build-mac-arm
 cmake -DCMAKE_OSX_ARCHITECTURES="x86_64;arm64" ..
 cmake --build . -j 8
 cmake --build . --target install
+popd
+
+mkdir -p framework
+pushd framework
+mkdir -p mac-arm
+pushd mac-arm
+mkdir -p otter.framework/Versions/A/Headers
+mkdir -p otter.framework/Versions/A/Resources
+ln -s A otter.framework/Versions/Current
+ln -s Versions/Current/Headers otter.framework/Headers
+ln -s Versions/Current/Resources otter.framework/Resources
+ln -s Versions/Current/otter otter.framework/otter
+lipo -create ../../build-mac-arm/install/lib/libotter.a -o otter.framework/Versions/A/otter
+cp -r ../../build-mac-arm/install/include/* otter.framework/Versions/A/Headers/
+sed -e 's/__NAME__/otter/g' -e 's/__IDENTIFIER__/com.duncan.otter/g' -e 's/__VERSION__/1.0/g' Info.plist > otter.framework/Versions/A/Resources/Info.plist
+popd
 popd
