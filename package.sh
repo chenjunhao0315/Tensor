0151a7760f74de799ae89e1776c47e622ef0c319
chenjunhao0315 <duncan11514@gmail.com>
2022-09-18 02:04:15 +0800

    fix build

--- package.sh
+++ package.sh
@@ -12,7 +12,7 @@ ln -s Versions/Current/Resources otter.framework/Resources
 ln -s Versions/Current/otter otter.framework/otter
 lipo -create ../../build-ios/install/lib/libotter.a ../../build-ios-sim/install/lib/libotter.a -o otter.framework/Versions/A/otter
 cp -r ../../build-ios/install/include/* otter.framework/Versions/A/Headers/
-sed -e 's/__NAME__/otter/g' -e 's/__IDENTIFIER__/com.duncan.otter/g' -e 's/__VERSION__/1.0/g' Info.plist > otter.framework/Versions/A/Resources/Info.plist
+sed -e 's/__NAME__/otter/g' -e 's/__IDENTIFIER__/com.duncan.otter/g' -e 's/__VERSION__/1.0/g' ../../Info.plist > otter.framework/Versions/A/Resources/Info.plist
 popd
 popd
 
@@ -28,7 +28,7 @@ ln -s Versions/Current/Resources otter.framework/Resources
 ln -s Versions/Current/otter otter.framework/otter
 lipo -create ../../build-mac/install/lib/libotter.a -o otter.framework/Versions/A/otter
 cp -r ../../build-mac/install/include/* otter.framework/Versions/A/Headers/
-sed -e 's/__NAME__/otter/g' -e 's/__IDENTIFIER__/com.duncan.otter/g' -e 's/__VERSION__/1.0/g' Info.plist > otter.framework/Versions/A/Resources/Info.plist
+sed -e 's/__NAME__/otter/g' -e 's/__IDENTIFIER__/com.duncan.otter/g' -e 's/__VERSION__/1.0/g' ../../Info.plist > otter.framework/Versions/A/Resources/Info.plist
 popd
 popd
 
@@ -44,6 +44,6 @@ ln -s Versions/Current/Resources otter.framework/Resources
 ln -s Versions/Current/otter otter.framework/otter
 lipo -create ../../build-mac-arm/install/lib/libotter.a -o otter.framework/Versions/A/otter
 cp -r ../../build-mac-arm/install/include/* otter.framework/Versions/A/Headers/
-sed -e 's/__NAME__/otter/g' -e 's/__IDENTIFIER__/com.duncan.otter/g' -e 's/__VERSION__/1.0/g' Info.plist > otter.framework/Versions/A/Resources/Info.plist
+sed -e 's/__NAME__/otter/g' -e 's/__IDENTIFIER__/com.duncan.otter/g' -e 's/__VERSION__/1.0/g' ../../Info.plist > otter.framework/Versions/A/Resources/Info.plist
 popd
 popd
